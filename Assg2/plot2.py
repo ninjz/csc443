479fbfea83d71bf6fcc6233d819635ba7d99f219
Amr <amrho_r@yahoo.com>
2014-02-23 23:26:57 -0500

    Create plot2.py

---
+++ Assg2/plot2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/pyhton
+from subprocess import Popen, PIPE, STDOUT
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+xPoints = []
+yPoints = []
+
+ans = 0.0
+
+
+for j in range(10):
+	proc = Popen(['./msort','file_in100','file_out100',str(j*1024), '2'],stdout=PIPE,stderr=STDOUT)
+	line = proc.stdout.readline()
+	#print line
+	ans = float(line.split(' ')[1])		
+	xPoints.append(100)
+	yPoints.append(ans)
+
+
+
+plt.plot(xPoints, yPoints)
+plt.title("memory_size vs time")
+plt.xlabel("memory_size")
+plt.ylabel("time in milliseconds")
+plt.savefig('memory_sizeVStime.png')
+
+
