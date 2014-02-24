a38004732212cbceb0dba68e55f804a5dc72b0f8
Amr <amrho_r@yahoo.com>
2014-02-23 20:50:20 -0500

    Update msort.h

--- Assg2/msort.h
+++ Assg2/msort.h
@@ -8,7 +8,7 @@
 //for fixed size records
 const int nbAttributes = 9;
 const int AttributeSize = 1;
-int page_size;
+//int page_size;
 
 typedef char Record[9];
 typedef struct {
@@ -87,32 +87,31 @@ void mk_filePage (FILE *out_fp, char* filename, int page_size);
 * creates runs of length `run_length` in
 * the `out_fp`.
 */
-//void mk_runs(FILE *in_fp, FILE *out_fp, long run_length); 
+void mk_runs(FILE *in_fp, FILE *out_fp, long run_length); 
 
 
-// class RunIterator {
-// 	int curr_pos;
-// 	int size;
-// 	char *buf;
-// 	Page page;
-// 	/**
-// 	* creates an iterator using the `buf_size` to
-// 	* scan through a run that starts at `start_pos`
-// 	* with length `run_length`
-// 	*/
-// 	RunIterator(FILE *fp, long start_pos, long run_length, long buf_size);
-// 	/**
-// 	* reads the next record. Returns null if
-// 	* iterator reads the end of the run.
-// 	*/
-// 	public:
-// 		Record* next();
-// };
+class RunIterator {
+	int curr_pos;
+	int size;
+	char *buf;
+	/**
+	* creates an iterator using the `buf_size` to
+	* scan through a run that starts at `start_pos`
+	* with length `run_length`
+	*/
+public:
+	RunIterator(FILE *fp, long start_pos, long run_length, long buf_size);
+	/**
+	* reads the next record. Returns null if
+	* iterator reads the end of the run.
+	*/
+		Record* next();
+};
 
-// void merge_runs(FILE *out_fp, 
-// 				RunIterator* iterators, 
-// 				int num_iterators,
-// 			    long buf_size);
+void merge_runs(FILE *out_fp, 
+				RunIterator* iterators, 
+				int num_iterators,
+			    long buf_size);
 
 
 
