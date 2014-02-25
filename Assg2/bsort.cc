2b45d8bf0f6ee0b72026ed87ccbe3a8b2ef3703c
Calvin <cchengtt@gmail.com>
2014-02-25 01:34:44 -0500

    bsort implementation completed added leveldb-1.15 for newer version.

--- Assg2/bsort.cc
+++ Assg2/bsort.cc
@@ -1,31 +1,33 @@
 #include <iostream>
+#include <cstdio>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "msort.h"
+#include "string.h"
 #include "leveldb/db.h"
  
+typedef char Record[9];
 
 
+using namespace std;
 
-
-/**
- * opens a database connection to "./leveldb_dir"
- */
-leveldb::DB *db;
-leveldb::Options options;
-options.create_if_missing = true;
-leveldb::Status status = leveldb::DB::Open(options, "./leveldb_dir", &db);
-
-
-leveldb::WriteOptions()
-leveldb::ReadOptions()
 
 
 int main(int argc, char ** argv){
 
+	/**
+	 * opens a database connection to "./leveldb_dir"
+	 */
+	leveldb::DB *db;
+	leveldb::Options options;
+	options.create_if_missing = true;
+	leveldb::Status status = leveldb::DB::Open(options, "./leveldb_dir", &db);
+
+
+	leveldb::WriteOptions();
+	leveldb::ReadOptions();
+
 	if (argc != 3){
-		printf("proper usage: bsort <input_file> <out_index>");
+		printf("proper usage: bsort <input_file> <out_index>\n");
 		return 0;
 
 	}
@@ -41,36 +43,31 @@ int main(int argc, char ** argv){
 	//read records
 	char c;
 	int unique_counter=0;
-	char * key = (char *) malloc (sizeof(Record));
-
-	//char key[9];
 	char unique_key[9 + sizeof(long)];
 	while (1){
+		Record record; 
 		//read one record at a time 
-
-		fread(record, sizeof(Record), 1, in_fp);
-		leveldb:Slice key = recordNumber;
-		leveldb:SLice value = record;
+		fread(record, sizeof(Record), 1, input_file);
+		leveldb::Slice value = record;
 		unique_counter ++;
-    	get_key(record, &key);
-   		memcpy(unique_key, key, 9); 
+   		memcpy(unique_key, record, 9); 
     	memcpy(unique_key + 9, &unique_counter, sizeof(long)); 
- 		db->Put(Slice(unique_key, sizeof(unique_key)), value);
+ 		db->Put(leveldb::WriteOptions(), leveldb::Slice(unique_key, sizeof(unique_key)), value); 
 
 		c = fgetc(input_file);
-		if (c==EOF) break;
+		if (c==EOF) goto cont;
 		ungetc(c, input_file);
-
 	}
 
+	cont: 
 	leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
 	for (it->SeekToFirst(); it->Valid(); it->Next()) {
 	    leveldb::Slice key = it->key();
 	    leveldb::Slice value = it->value();
-	    std::string key_str = key.ToString();
-	    std::string val_str = value.ToString();
-	    fwrite(key, sizeof(key), 1, out_index);
-	    cout << key_str << ": "  << val_str << endl;
+	    string key_str = key.ToString();
+	    string val_str = value.ToString();
+	    fwrite(key_str.c_str(), sizeof(Record), 1, out_index);
+	    cout << key_str << ":"  << val_str << endl;
 	}
 	assert(it->status().ok());  // Check for any errors found during the scan
 	delete it;
