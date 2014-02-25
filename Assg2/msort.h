469a9d6c53f390299cdfd3df8a86c67dab0efdc1
Amr <amrho_r@yahoo.com>
2014-02-25 01:45:48 -0500

    Update msort.h

--- Assg2/msort.h
+++ Assg2/msort.h
@@ -35,58 +35,6 @@ typedef struct {
 
 int cmpstr(void const *a, void const *b);
 
-/** 
- * Compute the number of bytes to serialize record
- */
-int fixed_len_sizeof(Record *record);
-
-/**
- * Serialize the record to a byte array to be stored in buf.
- */
-void fixed_len_write(Record *record, void *buf);
-
-
-/**
- * Deserializes from `size` bytes from the buffer, `buf`, and
- * stores the record in `record`.
- */
-void fixed_len_read(void *buf, int size, Record * record);
-
-/**
-* Initializes a page using the given slot size
-*/
-void init_fixed_len_page(Page *page, int page_size, int slot_size);
-
-/**
-* Calculates the maximal number of records that fit in a page
-*/ 
-int fixed_len_page_capacity(Page *page);
-
-/**
-* Calculate the free space (number of free slots) in the page
-*/ 
-int fixed_len_page_freeslots(Page *page);
-
-/**
-* Add a record to the page
-* Returns:
-* record slot offset if successful,
-* -1 if unsuccessful (page full)
-*/
-int add_fixed_len_page(Page *page, Record *r);
-
-/**
-* Write a record into a given slot.
-*/
-void write_fixed_len_page(Page *page, int slot, Record *r);
-
-/**
-* Read a record from the page from a given slot.
-*/
-void read_fixed_len_page(Page *page, int slot, Record *r);
-
-
-void mk_filePage (FILE *out_fp, char* filename, int page_size);
 
 /**
 * creates runs of length `run_length` in
@@ -100,7 +48,7 @@ class RunIterator {
 	public:
 	int curr_pos;
 	int size;
-	char *buf;
+	char **buf;
 	int start_pos;
 	char * page;
 	int page_offset;
