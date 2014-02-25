664ebed5ec0ae8b02e653b080a39ccbad73988b2
Amr <amrho_r@yahoo.com>
2014-02-24 22:48:29 -0500

    Update msort.cc

--- Assg2/msort.cc
+++ Assg2/msort.cc
@@ -1,38 +1,128 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
 
-#include "msort.h"
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fstream>
+
+//for fixed size records
+const int nbAttributes = 9;
+const int AttributeSize = 1;
+int num_iterators;
+long run_length;
+long page_size;
+long nbPages;
+long buf_size;
+
+
+typedef char Record[9];
+typedef struct {
+    void *data;
+    int page_size;
+ 	int slot_size;
+} Page;
+
+typedef int PageID;
+typedef char Record[9];
+     
+typedef struct {
+	int page_offset;
+	int slot;
+} RecordID;
 
 
 
 
+int cmpstr(void const *a, void const *b);
 
-int main(int argc, char * argv[]){
+/** 
+ * Compute the number of bytes to serialize record
+ */
+int fixed_len_sizeof(Record *record);
 
-	if (argc != 5){
-		printf("proper usage: msort <input_file> <out_file> <mem_capacity> <k>\n");
-		return 0;
+/**
+ * Serialize the record to a byte array to be stored in buf.
+ */
+void fixed_len_write(Record *record, void *buf);
 
-	}
 
-	FILE * in_fp, *out_fp;
-	int mem_capacity, k;
+/**
+ * Deserializes from `size` bytes from the buffer, `buf`, and
+ * stores the record in `record`.
+ */
+void fixed_len_read(void *buf, int size, Record * record);
 
-	if (! (k = atoi(argv[4]))){
-		printf("Error converting k to integer\n");
+/**
+* Initializes a page using the given slot size
+*/
+void init_fixed_len_page(Page *page, int page_size, int slot_size);
 
-	}
-	if (! (mem_capacity = atoi(argv[3]))){
-		printf("Error converting mem_capcity to int\n");
+/**
+* Calculates the maximal number of records that fit in a page
+*/ 
+int fixed_len_page_capacity(Page *page);
 
-	}
-	int page_size = mem_capacity / k;
+/**
+* Calculate the free space (number of free slots) in the page
+*/ 
+int fixed_len_page_freeslots(Page *page);
 
-	mk_filePage(in_fp, argv[1],page_size);
+/**
+* Add a record to the page
+* Returns:
+* record slot offset if successful,
+* -1 if unsuccessful (page full)
+*/
+int add_fixed_len_page(Page *page, Record *r);
 
-	return 0;
+/**
+* Write a record into a given slot.
+*/
+void write_fixed_len_page(Page *page, int slot, Record *r);
 
-}
+/**
+* Read a record from the page from a given slot.
+*/
+void read_fixed_len_page(Page *page, int slot, Record *r);
+
+
+void mk_filePage (FILE *out_fp, char* filename, int page_size);
+
+/**
+* creates runs of length `run_length` in
+* the `out_fp`.
+*/
+void mk_runs(FILE *in_fp, FILE *out_fp, long run_length); 
+
+
+class RunIterator {
+
+	public:
+	int curr_pos;
+	int size;
+	char *buf;
+	int start_pos;
+	char * page;
+	int page_offset;
+	/**
+	* creates an iterator using the `buf_size` to
+	* scan through a run that starts at `start_pos`
+	* with length `run_length`
+	*/
+
+	RunIterator(FILE *fp, long start_pos, long run_length, long buf_size);
+	/**
+	* reads the next record. Returns null if
+	* iterator reads the end of the run.
+	*/
+		char * next();
+};
+
+void merge_runs(FILE *out_fp, 
+				RunIterator* iterators, 
+				int num_iterators,
+			    long buf_size);
+
+
+
+#endif
